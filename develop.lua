ff19de77d3c74e30dfa1bb4899825fb9f0791e08
Rampage7675 <tiktokyounoyatuya@gmail.com>
2025-03-24 07:01:27 +0900

    Update develop.lua

--- develop.lua
+++ develop.lua
@@ -1,3 +1,14 @@
+--[[Nyanko_Script DS]]
+local req = gg.makeRequest("https://github.com/Rampage7675/Nyanko_Script/raw/refs/heads/main/funcs/security.lua");
+if req and req.code == 200 then
+    exe = load(req.content)();
+    exe(gg.makeRequest("https://github.com/Rampage7675/Nyanko_Script/raw/refs/heads/main/funcs/setup.lua").content);
+    exe(gg.makeRequest("https://github.com/Rampage7675/Nyanko_Script/raw/refs/heads/main/funcs/library.lua").content)
+else
+    print("インターネットに接続してください");
+    return false;
+end
 
-
---code
+--main code
+print("Base value: "..baset..("(%s)"):format(basex));
+print("Base address: "..("0x%08x"):format(base & 0xffffffff));
